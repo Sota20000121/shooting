b09372e272e1f93396e9ce64539279d34a1f8ce5
Sota20000121 <ntv-350-pvg@e-net.nara.jp>
2025-10-17 06:19:54 +0000

    プログラムの分割 敵の出現

--- game/js/main.js
+++ game/js/main.js
@@ -1,14 +1,11 @@
+import { player,initPlayer,drawPlayer} from "./player.js";
+import { enemies,spawanEnemies} from "./enemies.js";
+
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
-const player = {
-    x: canvas.width / 2 - 15,
-    y: canvas.height - 60,
-    width: 30,
-    height: 30,
-    color : "#7700ff",
-    life : 3,
-};
+initPlayer(canvas);
+spawanEnemies(canvas);
 
 const bullets = [];
 const BULLET_SPEED = -5;
@@ -56,13 +53,19 @@ function draw(){
     ctx.fillStyle = "#000000";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    ctx.fillStyle = player.color;
-    ctx.fillRect(player.x, player.y, player.width, player.height);
+    drawPlayer(ctx);
 
     ctx.fillStyle = "#ff00f2ff";
     for (let i = 0; i < bullets.length ; i++) {
         const bullet = bullets[i];
         ctx.fillRect(bullet.x, bullet.y, bullet.width, bullet.height);
+
+    }
+
+    ctx.fillStyle = "#ff00f2ff";
+    for (let i = 0; i < enemies.length ; i++) {
+        const enemy = enemies[i];
+        ctx.fillRect(enemy.x, enemy.y, enemy.width, enemy.height);
 
     }
 
